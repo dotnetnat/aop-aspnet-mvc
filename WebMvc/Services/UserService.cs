4ea467883d57a0a716c2feef764c7f74de75dc29
Narongchai Thongkongkeaw <nat@freshket.co>
2017-01-23 07:59:03 +0700

    update nlog

--- WebMvc/Services/UserService.cs
+++ WebMvc/Services/UserService.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Castle.Core;
+using WebMvc.Interceptors;
 using WebMvc.Interfaces;
 
 namespace WebMvc.Services
 {
+   
     public class UserService: IUserService
     {
         public bool Login(string email, string password)
